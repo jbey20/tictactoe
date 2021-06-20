a1d0685b484a0a123433142e8fa987dc6245c675
Joshua Beychok <joshbeychok3@gmail.com>
2021-06-19 23:57:49 -0400

    Added end of game popup, reset function, top menu bar with inputs and start button

--- scripts.js
+++ scripts.js
@@ -1,10 +1,21 @@
+//Enter functionality to add Player's names
+  //popup for 
+//responsiveness & looks
+//AI
+
+
 const gameBoard = (() => {
   const gameState = [,,,,,,,];
   let playerTurn = "O";
-  const win = 
-    [0,0,0, //row 0,1,2
-     0,0,0, //column 3,4,5
-     0,0];  //diagonal 6,7
+  let moveCounter = 0;
+  const win = [0,0,0,0,0,0,0,0];  
+  let xPlayer;
+  let oPlayer;
+
+  const assignPlayer = (player1, player2) => {
+    xPlayer = player1;
+    oPlayer = player2;
+  } 
 
   const toggleTurn = () => {
     if (playerTurn == 'O') playerTurn = 'X';
@@ -12,11 +23,13 @@ const gameBoard = (() => {
   }
 
   const checkWin = () => {
-    if (win.includes(3) || win.includes(-3)) return true;
-    else return false;
+    if (win.includes(3) || win.includes(-3)) return 1;
+    else if (moveCounter == 9) return 2;
+    else return 3;
   }
 
   const increment = (cell) => {
+    moveCounter++;
     const x = playerTurn == 'X' ? 1 : -1; 
     if (cell == 0) {
       win[0] += x;
@@ -71,8 +84,14 @@ const gameBoard = (() => {
       increment(cell);
       gameState[cell] = playerTurn;
       displayController.fillGrid();
-      console.log(checkWin());
-      if (checkWin()) console.log(`${playerTurn} wins!`);
+      if (checkWin() == 1) {
+        const msg = `${playerTurn} wins!`;
+        displayController.endGame(msg);
+      }
+      else if (checkWin() == 2) {
+        const msg = 'Draw!'
+        displayController.endGame(msg);
+      }
     }
   }
 
@@ -80,29 +99,81 @@ const gameBoard = (() => {
     return gameState;
   }
 
+  const startGame = () => {
+    displayController.fillGrid();
+    displayController.initialize();
+  }
+
   const resetGame = () => {
     for (let i = 0; i < gameState.length; i++) {
       gameState[i] = null;
     }
-    console.log(gameState);
+    for (let i = 0; i < win.length; i++) {
+      win[i] = 0;
+    }
+    playerTurn = 'O';
+    moveCounter = 0;
     displayController.emptyGrid();
   }
 
   return {
     getGameState,
     makePlay,
+    startGame,
     resetGame
   };
 })();
 
 const displayController = (() => {
   const initialize = () => {
-    document.querySelectorAll('.container div').forEach( div => {
-      div.addEventListener('click', e => {
-        gameBoard.makePlay(parseInt(e.target.id.slice(-1)))
-      })
+    document.querySelectorAll('.board-container div').forEach( item => {
+        item.addEventListener('click', e => {
+          const div = e.target;
+          gameBoard.makePlay(parseInt(div.id.slice(-1)));
+      });
     });
   }
+
+  const endGame = (text) => {
+    console.log('ending game');
+    var modal = document.getElementById('myModal');
+    // Get the close button
+    var btnClose = document.getElementById("closeModal");
+    // Get the main container and the body
+    var body = document.getElementsByTagName('body');
+    var container = document.getElementById('myContainer');
+    const msg = document.getElementById('msg');
+    msg.textContent = text;
+
+    // Open the modal
+    modal.className = "Modal is-visuallyHidden";
+    setTimeout(function() {
+      container.className = "board-container is-blurred";
+      modal.className = "Modal";
+    }, 100);
+    container.parentElement.className = "ModalOpen";
+
+    // Close the modal
+    btnClose.onclick = function() {
+      gameBoard.resetGame();
+      modal.className = "Modal is-hidden is-visuallyHidden";
+      body.className = "";
+      container.className = "board-container";
+      container.parentElement.className = "";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        gameBoard.resetGame();
+        modal.className = "Modal is-hidden";
+        body.className = "";
+        container.className = "board-container";
+        container.parentElement.className = "";
+      }
+    }
+  }
+
   const writeToDOM = (selector, content) => {
     document.querySelector(selector).textContent = content;
   };
@@ -125,19 +196,20 @@ const displayController = (() => {
   return {
     fillGrid,
     initialize,
-    emptyGrid
+    emptyGrid,
+    endGame,
   };
 })();
 
-// const playerFactory = (name, symbol) => {
-//   const name = name;
-//   const symbol = symbol;
-//   return {
-//     name,
-//     symbol
-//   }
-// }
+const playerFactory = (name, symbol) => {
+  const getName = () => name;
+  const getSymbol = () => symbol;
+  return {
+    getName,
+    getSymbol
+  }
+}
 
-displayController.fillGrid();
-displayController.initialize();
+    
+
 
